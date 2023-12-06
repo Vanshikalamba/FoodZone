83713e0e51655c73a6697e485c0a7f36173b9deb
vanshika lamba <vanshikalmb@gmail.com>
2023-12-06 23:11:33 +0530

    added live api and shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 //import resObj from "../utils/mockData";
 import resList from "../utils/dummyData";
 //only let can be updated not const
 const Body = () => {
-  const [listRes, setlistRes] = useState(resList);
+  const [listRes, setlistRes] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const res = await data.json();
+    console.log(res);
+    //optional chaining
+    setlistRes(
+      res.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }; //res.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+  if (listRes.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
